4f990bc1f069a3ebd32e49d138a52022aeedf869
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2022-12-14 11:26:53 +0200

    Increment the counter by 5 per click

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var numOfClicks = 0;
 
 $(document).ready(function () {
   $("#click-button").on("click", function () {
-    numOfClicks += 3;
+    numOfClicks += 5;
     document.getElementById("click-counter").innerHTML = numOfClicks;
   });
 });
